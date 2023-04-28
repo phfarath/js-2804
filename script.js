a418ffa672f47f919d2cac1d7c99cb60df49b508
phfarath <pedropontesfarath@gmail.com>
2023-04-28 10:47:54 -0300

    js-2804

---
+++ script.js
@@ -0,0 +1,10 @@
+// declarando a msg 
+let msg = document.getElementById('msg')
+
+// criar obj com array 
+const usuarios =[
+    {nome:"PEDRO", idade:18},
+    {nome:"CLARA", idade:19},
+    {nome:"JDHKJADHK", idade:20},
+
+];
